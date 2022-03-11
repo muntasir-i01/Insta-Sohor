14b0feb1bba0de308f62b2a1880ad5c813a7fb24
muntasir-i01 <you@example.communtasirm55@gmail.com>
2022-03-11 09:41:21 +0600

    changed const variable name posts -> reportedPosts inside displayReportedPosts function

--- js/app.js
+++ js/app.js
@@ -46,7 +46,7 @@ const switchTab = (id) => {
         document.getElementById( "reported" ).style.display = "none";
 
         displayLikedPosts();
-    } else {
+    } else if(id === "reported") {
         document.getElementById( "reported" ).style.display = "block";
         document.getElementById( "posts" ).style.display = "none";
         document.getElementById( "liked" ).style.display = "none";
@@ -157,7 +157,7 @@ const displayLikedPosts = () => {
 
 const displayReportedPosts = () => {
     const reportedPosts = getReportedPosts();
-    posts.forEach((post) => {
+    reportedPosts.forEach((post) => {
         const div = createPost(post);
         document.getElementById( "reported" ).appendChild(div);
     });
